f9448d67437b7cdb64f68b9a7ea9e00dbd54b44c
Szymon <seltinxx@gmail.com>
2021-02-24 18:13:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,109 @@
+# from math import *
+#
+# a = "12345"
+# b = "Ala ma kota"
+# print(a+b)
+# x = 10
+# y = 5
+# z = 2
+# dzielenie = y/z
+# print(dzielenie)
+# print(type(dzielenie))
+#
+# dodawanie = x+y
+# dzielenie_calkowite = y // z
+# potega =  x ** z
+# print(dodawanie)
+# print(dzielenie_calkowite)
+# print(potega)
+#
+# potega = pow(8,2)
+# print(potega)
+#
+# pierwiastek = sqrt(64)
+# print(pierwiastek)
+#
+# x += 2
+# print(x)
+
+
+# Zadanie 1
+
+zadanie1 = "Zadanie 1"
+print(zadanie1)
+
+a = 5
+b = float(6.5)
+tekst = "Ala ma kota"
+print(a)
+print(b)
+print(tekst)
+
+znakiod = "----------------------------------------------------"
+print(znakiod)
+
+# Zadanie 2
+
+zadanie2 = "Zadanie 2"
+print(zadanie2)
+
+x = 2
+y = 4
+z = 6
+
+dodawanie = x+y
+print(dodawanie)
+odejmowanie = z-y
+print(odejmowanie)
+mnozenie = x*y
+print(mnozenie)
+dzielenie_calkowite = 4//2
+print(dzielenie_calkowite)
+dzielenie = z/y
+print(dzielenie)
+potegowanie = x ** 2
+print(potegowanie)
+potegowanie_na_zmiennych = x ** y
+print(potegowanie_na_zmiennych)
+
+znakiod = "----------------------------------------------------"
+print(znakiod)
+
+# Zadanie 3
+
+zadanie3 = "Zadanie 3"
+print(zadanie3)
+
+a1=10
+print(a1)
+a1+=2
+print(a1)
+a2=12
+print(a2)
+a2-=2
+print(a2)
+a3=10
+print(a3)
+a3*=2
+print(a3)
+a4=100
+print(a4)
+a4/=2
+print(a4)
+a5=2
+print(a5)
+a5**=4
+print(a5)
+a6=11
+print(a6)
+a6%=2
+print(a6)
+
+znakiod = "----------------------------------------------------"
+print(znakiod)
+
+# Zadanie 4
+
+zadanie4 = "Zadanie 4"
+print(zadanie4)
+
